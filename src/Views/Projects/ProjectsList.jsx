912af544a68e0ff9eba4ba4cc467e90657a3ecaf
Juan Carlos Cruz <j.cruzb@outlook.com>
2025-03-02 13:04:54 +0100

    Fix: Projects list views

--- src/Views/Projects/ProjectsList.jsx
+++ src/Views/Projects/ProjectsList.jsx
@@ -3,11 +3,13 @@ import Paper from '@mui/material/Paper';
 import { Button, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getProjects } from '../../Services/ProjectService'; // Asegúrate de tener esta función implementada
+import { getProjects } from '../../Services/ProjectService';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'title', headerName: 'Nombre del proyecto', width: 200 },
+  { field: 'id', headerName: 'ID', width: 70 }, // Mantén la columna para el manejo interno
+  { field: 'savingsOwner', headerName: 'Ahorrador', width: 150 }, // Mantén la columna para el manejo interno
+  { field: 'title', headerName: 'Nombre del proyecto', width: 150 },
+  { field: 'savingsGenerated', headerName: 'Ahorros (MWh)', width: 150 }, // Mantén la columna para el manejo interno
   {
     field: 'auctionStatus',
     headerName: 'Estado de la subasta',
@@ -18,13 +20,17 @@ const columns = [
 const ProjectsList = () => {
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [columnVisibility, setColumnVisibility] = useState({ id: false }); // Estado para manejar la visibilidad
 
   useEffect(() => {
     getProjects()
       .then(response => {
+        console.log(response)
         const rows = response.map(project => ({
-          id: project._id, // Asegúrate de que _id existe o usa project.id
+          id: project._id,
+          savingsOwner:project.savingsOwner,
           title: project.title,
+          savingsGenerated:project.savingsGenerated,
           auctionStatus:
             project.auction && project.auction.length > 0
               ? project.auction[0].closed
@@ -46,17 +52,21 @@ const ProjectsList = () => {
   }
 
   return (
-    <Container sx={{marginTop:'1vh'}}>
-      <Button  component={Link} to="/projectsCreateForm" variant="contained">
+    <Container sx={{ marginTop: '1vh' }}>
+      <Button component={Link} to="/projectsCreateForm" variant="contained">
         Agregar Proyecto
       </Button>
       <Paper sx={{ height: 400, width: '100%', mt: 2 }}>
         <DataGrid
           rows={projects}
           columns={columns}
-          initialState={{ pagination: { paginationModel: { page: 0, pageSize: 5 } } }}
+          initialState={{
+            pagination: { paginationModel: { page: 0, pageSize: 5 } },
+          }}
+          columnVisibilityModel={columnVisibility} // Se oculta la columna ID
+          onColumnVisibilityModelChange={(newModel) => setColumnVisibility(newModel)}
           pageSizeOptions={[5, 10]}
-          checkboxSelection
+          disableSelectionOnClick
           sx={{ border: 0 }}
         />
       </Paper>
